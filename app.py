04a9abcdfe995d3c12790c8af02ade1f63e75e42
Allie Robinson <allierob93@gmail.com>
2023-02-28 23:40:30 -0600

    :sparkles: flask app with users, brags, and bragtags models

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 from flask import Flask
 from flask_cors import CORS
+from models import db, migrate
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-CORS(app)
+    CORS(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    @app.route('/')
+    def hello_world():
+        return 'weeeeeeew'
+
+    return app
+
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,37 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80), nullable=False)
+    last_name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return f'{self.id} {self.first_name} {self.last_name} {self.email}'
+
+class Brag(db.Model):
+    __tablename__ = 'brags'
+    id = db.Column(db.Integer, primary_key=True)
+    headline = db.Column(db.String(120), nullable=False)
+    content = db.Column(db.Text(), nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    brag_tags = db.relationship('bragtags', backref='brags', lazy=True)
+
+    def __repr__(self):
+        return f'{self.id} {self.headline} {self.content} {self.timestamp} {self.user_id}'
+
+class BragTag(db.Model):
+    __tablename__ = 'bragtags'
+    id = db.Column(db.Integer, primary_key=True)
+    tag_name = db.Column(db.String(120), nullable=False)
+    brags = db.relationship('brags', secondary='bragtags', backref='bragtags')
+
+    def __repr__(self):
+        return f'{self.id} {self.tag_name}'
